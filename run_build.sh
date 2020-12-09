4b55638db02b0bff5f8145dff930a1962a6b71a5
Travis Hoffman <travis@medshorts.com>
2020-12-09 10:37:55 -0800

    Hardcode region and account id
    ref: ms2.0/822

--- run_build.sh
+++ run_build.sh
@@ -3,7 +3,7 @@
 echo "Conjuring environment files for all matched deployment targets..."
 python build-tools/gen_build_env.py $CODEBUILD_WEBHOOK_HEAD_REF
 
-$(aws ecr get-login-password --region $AWS_DEFAULT_REGION | docker login --username AWS --password-stdin $AWS_ACCOUNT_ID.dkr.ecr.$AWS_DEFAULT_REGION.amazonaws.com)
+$(aws ecr get-login-password --region us-west-2 | docker login --username AWS --password-stdin 392331991905.dkr.ecr.us-west-2.amazonaws.com)
 . api/.test.env
 ./build/test.sh || exit 1
 
