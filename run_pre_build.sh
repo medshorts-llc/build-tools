613b39bbaf947eed2866a78d063b4c86c40dd30d
Travis Hoffman <travis@medshorts.com>
2020-12-08 20:07:05 -0800

    Move some buildspec commands into shell script
    ref: ms2.0/822

--- run_pre_build.sh
+++ run_pre_build.sh
@@ -1,5 +1,13 @@
 #!/usr/bin/env bash
 
 echo "Run pre_build hook..."
+
 cd $CODEBUILD_SRC_DIR
+
+chmod +x ./build-tools/run_pre_build.sh
+chmod +x ./build-tools/run_build.sh
+chmod +x ./build-tools/run_test.sh
+chmod +x ./build/build.sh
+chmod +x ./build/deploy.sh
+
 bash build/pre_build.sh
