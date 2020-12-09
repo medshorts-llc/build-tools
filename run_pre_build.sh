6b98c26379a1236d1fdeb37949cf2592673bba7a
Travis Hoffman <travis@medshorts.com>
2020-12-08 20:16:27 -0800

    Auth ecr in run_pre_build
    ref: ms2.0/822

--- run_pre_build.sh
+++ run_pre_build.sh
@@ -4,10 +4,11 @@ echo "Run pre_build hook..."
 
 cd $CODEBUILD_SRC_DIR
 
+$(aws ecr get-login-password --region $AWS_DEFAULT_REGION | docker login --username AWS --password-stdin $AWS_ACCOUNT_ID.dkr.ecr.$AWS_DEFAULT_REGION.amazonaws.com)
+
 chmod +x ./build-tools/run_build.sh
 chmod +x ./build-tools/run_test.sh
 chmod +x ./build/build.sh
 chmod +x ./build/deploy.sh
 chmod +x ./build/pre_build.sh
-
 bash build/pre_build.sh
