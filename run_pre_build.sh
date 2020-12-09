d7e4387740156d239371028c06b509348ba83523
Travis Hoffman <travis@medshorts.com>
2020-12-08 20:10:46 -0800

    Run run_pre_build with bash explicitly
    ref: ms2.0/822

--- run_pre_build.sh
+++ run_pre_build.sh
@@ -4,10 +4,10 @@ echo "Run pre_build hook..."
 
 cd $CODEBUILD_SRC_DIR
 
-chmod +x ./build-tools/run_pre_build.sh
 chmod +x ./build-tools/run_build.sh
 chmod +x ./build-tools/run_test.sh
 chmod +x ./build/build.sh
 chmod +x ./build/deploy.sh
+chmod +x ./build/pre_build.sh
 
 bash build/pre_build.sh
