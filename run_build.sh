545df28cac6fe19d08c0a6861328809dc4278f11
Travis Hoffman <travis@medshorts.com>
2021-02-15 11:16:23 -0800

    Changes for github actions

--- run_build.sh
+++ run_build.sh
@@ -1,14 +1,14 @@
 #!/usr/bin/env bash
 
 echo "Conjuring environment files for all matched deployment targets..."
-python build-tools/gen_build_env.py $CODEBUILD_WEBHOOK_HEAD_REF
+python build-tools/gen_build_env.py $GITHUB_REF
 
 $(aws ecr get-login-password --region us-west-2 | docker login --username AWS --password-stdin 392331991905.dkr.ecr.us-west-2.amazonaws.com)
+
 . api/.test.env
-./build/test.sh || exit 1
+# ./build/test.sh || exit 1
 
 for fname in **/.build.*.env; do
-    cd $CODEBUILD_SRC_DIR
     if [ -f "$fname" ]; then
 
         set -a
@@ -18,17 +18,14 @@ for fname in **/.build.*.env; do
         echo Running build hook
         ./build/build.sh
 
-        cd $CODEBUILD_SRC_DIR
-
         set -a
         . $fname
         set +a
 
         if [ "$SHOULD_DEPLOY" = "YES" ]; then
             echo "Running deploy hook"
-            ./build/deploy.sh
+            # ./build/deploy.sh
         fi
     fi
     SHOULD_DEPLOY="NO"
-    cd $CODEBUILD_SRC_DIR
 done
--- run_pre_build.sh
+++ run_pre_build.sh
@@ -2,8 +2,6 @@
 
 echo "Run pre_build hook..."
 
-cd $CODEBUILD_SRC_DIR
-
 chmod +x ./build-tools/run_build.sh
 chmod +x ./build-tools/run_test.sh
 chmod +x ./build/build.sh
