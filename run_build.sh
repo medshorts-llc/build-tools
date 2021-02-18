85b332fc0a8ce96ae922e29573114f9aa4d764a6
Travis Hoffman <travis@medshorts.com>
2021-02-17 16:01:23 -0800

    Remove .test.env source

--- run_build.sh
+++ run_build.sh
@@ -5,9 +5,6 @@ python3 build-tools/gen_build_env.py $GITHUB_REF
 
 $(aws ecr get-login-password --region us-west-2 | docker login --username AWS --password-stdin 392331991905.dkr.ecr.us-west-2.amazonaws.com)
 
-. api/.test.env
-# ./build/test.sh || exit 1
-
 for fname in **/.build.*.env; do
     if [ -f "$fname" ]; then
 
