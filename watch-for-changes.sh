d21cc1fc8d53d0423cef0f9ec4390f9397097207
Travis Hoffman <travis@medshorts.com>
2021-07-09 10:14:01 -0700

    More efficient change watching

--- watch-for-changes.sh
+++ watch-for-changes.sh
@@ -1,5 +1,6 @@
 while true
 do
-    inotifywait -e modify -e attrib -e move -e create -e delete --exclude '.*(\.pyc)' -r ./ && pkill -HUP gunicorn
+    find . | grep -v .git | grep py > /tmp/watchfiles.txt
+    inotifywait -e modify -e attrib -e move -e create -e delete --fromfile /tmp/watchfiles.txt && pkill -HUP gunicorn
     sleep 1
 done
