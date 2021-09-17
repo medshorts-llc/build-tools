6c9bd839831c72e46e60831e7da92ab6ffcf0b05
Travis Hoffman <travis@medshorts.com>
2021-09-16 21:29:38 -0700

    TestCraft support

--- run_build.sh
+++ run_build.sh
@@ -5,6 +5,9 @@ python3 build-tools/gen_build_env.py $GITHUB_REF
 
 $(aws ecr get-login-password --region us-west-2 | docker login --username AWS --password-stdin 392331991905.dkr.ecr.us-west-2.amazonaws.com)
 
+npm install -g tc-cli
+
+trigger_target=""
 for fname in **/.build.*.env; do
     if [ -f "$fname" ]; then
 
@@ -22,7 +25,37 @@ for fname in **/.build.*.env; do
         if [ "$SHOULD_DEPLOY" = "YES" ]; then
             echo "Running deploy hook"
             ./build/deploy.sh
+
+            if [ "$TESTCRAFT" = "YES" ]; then
+                echo "Running TestCraft suite..."
+
+                result=$(tc-cli run -u travis@medshorts.com -t DFA12339B3934E65A24BE08FDC67086E -p "Production" -v "Base" -s "$TESTCRAFT_SUITE" -l "Chrome 1920x1080" -e "$TESTCRAFT_ENV" | grep "Test Result: Success")
+
+                if [ -z "$result" ]; then
+                    echo "Tests failed! Post-test deploy aborted."
+                else
+                    echo "Tests passed! Post-test deploying to $TRIGGER_TARGET targets."
+                    python3 build-tools/gen_build_env.py $TRIGGER_TARGET
+
+                    set -a
+                    . **/.build.$TRIGGER_TARGET_ENV.env
+                    set +a
+
+                    echo Running build hook
+                    ./build/build.sh
+
+                    set -a
+                    . **/.build.$TRIGGER_TARGET_ENV.env
+                    set +a
+
+                    if [ "$SHOULD_DEPLOY" = "YES" ]; then
+                        echo "Running deploy hook"
+                        ./build/deploy.sh
+                    fi
+                fi
+            fi
         fi
     fi
+
     SHOULD_DEPLOY="NO"
 done
--- run_test.sh
+++ run_test.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/env bash
 
-echo "SHOULD_TEST is $SHOULD_TEST"
-if [ "$SHOULD_TEST" = "YES" ]; then
-    echo "RUNNING TEST"
-    bash build/test.sh
-fi
+# if [ "$TESTCRAFT" = "YES" ]; then
+#     echo "RUNNING TEST"
+#     bash build/test.sh
+# fi
